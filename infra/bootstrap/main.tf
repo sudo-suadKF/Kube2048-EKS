eeaa3c8e8a1db4d853e32f238837ec0d3853a913
sudo-suadKF <suadfehric@outlook.com>
2026-01-31 23:30:45 +0100

    Add: declared s3 module in main.tf

--- infra/bootstrap/main.tf
+++ infra/bootstrap/main.tf
@@ -3,3 +3,12 @@ module "ecr" {
   ecr-repo-name = var.ecr-repo-name
   ecr-repo-tag-mutability = var.ecr-repo-tag-mutability
 }
+
+module "s3" {
+  source = "./modules/s3"
+  s3-bucket-name = var.s3-bucket-name
+  s3-bucket-object-ownership = var.s3-bucket-object-ownership
+  s3-bucket-sse-algorithm = var.s3-bucket-sse-algorithm
+  s3-bucket-tag = var.s3-bucket-tag
+  s3-bucket-versioning-status = var.s3-bucket-versioning-status
+}
